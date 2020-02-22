9c7827e4ecbee9b860822de07148b99b41025a43
Dmitry Varin <varindv@gmail.com>
2020-02-23 01:30:42 +0300

    Solve task 5 - beta1

--- lab_01_0_5/main.c
+++ lab_01_0_5/main.c
@@ -4,13 +4,14 @@ long int nod(long int a, long int b);
 int main()
 {
     long int a, b, x;
+    printf("Input two nubmers: ");
     if (scanf("%ld%ld", &a, &b) != 2 || (a < 1) || (b < 1)) 
     {
         printf("Incorrect input\n");
         return EXIT_FAILURE;
     }
     x = nod(a, b);
-    printf("%ld", x);
+    printf("NOD of two numbers is %ld", x);
     return EXIT_SUCCESS;
 }
 
@@ -30,5 +31,4 @@ long int nod(long int a, long int b)
            a = tmp;
        }
     return b;
-    
 }
