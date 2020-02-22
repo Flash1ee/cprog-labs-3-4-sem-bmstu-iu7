c3d148cae507fc382362804f432f3756ac6c013d
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-02-22 13:48:23 +0300

    Change type of data

--- lab_01_0_3/main.c
+++ lab_01_0_3/main.c
@@ -8,14 +8,13 @@ int main(void)
     double normal_weight, index_mass;
     printf("Input high, chest_len,mass: ");
     if (scanf("%lf", &high) != 1 || scanf("%lf", &chest_len) != 1 || scanf("%lf", &mass) != 1
-        || (high <= 0) || (chest_len < 0) || (mass < 0))
-    {
+        || (high <= 0) || (chest_len < 0) || (mass < 0)) {
         printf("Incorrect input");
         return EXIT_FAILURE;
     }
     normal_weight = high * chest_len / 240;
     high = high / 100;
-    index_mass = mass / (high  * high);
+    index_mass = mass / (high * high);
     printf("Normal weight = %.5lf,Index mass = %.5lf\n", normal_weight, index_mass);
     return EXIT_SUCCESS;
 }
