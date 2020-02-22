3a95342fb9f2ada845821d2215630a7167ed4ab8
Dmitry Varin <varindv@gmail.com>
2020-02-23 00:49:47 +0300

    Solve task 5 - alpha

---
+++ lab_01_0_5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+long int nod(long int a, long int b);
+int main()
+{
+    long int a,b;
+    if (scanf("%ld%ld",&a,&b)!=2 || (a<1) || (b<1)) 
+    {
+        printf("Incorrect input");
+        return EXIT_FAILURE;
+    }
+    printf("%ld",nod(a,b));
+    return EXIT_SUCCESS;
+}
+
+long int nod(long int a, long int b)
+{
+    if (a>b)
+    {   
+        if (a % b == 0)
+            return b;
+        return a%b;
+    }
+    else
+    {   if (b % a == 0)
+            return a;
+        return b%a;
+    }   
+}
