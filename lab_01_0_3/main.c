38bda706400e016a55f9a59757716a0cccf1b6cb
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-02-22 15:06:08 +0300

    Change type of data

--- lab_01_0_3/main.c
+++ lab_01_0_3/main.c
@@ -6,7 +6,13 @@ int main(void)
     setbuf(stdout, NULL);
     double high, chest_len, mass;
     double normal_weight, index_mass;
-    scanf("%lf%lf%lf", &high, &chest_len, &mass);
+    printf("Input high, chest_len,mass: ");
+    if (scanf("%lf", &high) != 1 || scanf("%lf", &chest_len) != 1 || scanf("%lf", &mass) != 1
+        || (high <= 0) || (chest_len < 0) || (mass < 0)) 
+    {
+        printf("Incorrect input\n");
+        return EXIT_FAILURE;
+    }
     normal_weight = high * chest_len / 240;
     high = high / 100;
     index_mass = mass / (high * high);
