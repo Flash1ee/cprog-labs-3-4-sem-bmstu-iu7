deb84260ec207b3e2db0d8d573fcc2e2a1fd18d2
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-02-24 00:54:16 +0300

    Task 7 -alpha

---
+++ lab_01_3_7/main.c
@@ -0,0 +1,36 @@
+/*Вычислить с точность eps:
+− приближенное значение функции s(x);
+− точное значение функции f(x);
+− абсолютную f(x) -s(x) и относительную (f(x) -s(x)) / f(x) ошибки приближенного значения.
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double calculate_function(double x);
+int main(void)
+{
+    double x, eps,cur_x;
+    long int k=0;
+    double sum = 0, func_res, abs_error, relative_error;
+    if (scanf("%lf%lf", &x, &eps) != 2 || eps < 0)
+    {
+        printf("Incorrect input\n");
+        return EXIT_FAILURE;
+    }
+    cur_x = x;
+    while (fabs(cur_x)>eps)
+    {
+        k = k+1;
+        sum = sum + cur_x;
+        cur_x = cur_x*(-1)*(x*x)*(2*k-1)/(2*k+1);
+    }
+    func_res = atan(x);
+    abs_error = fabs(func_res - sum);
+    relative_error = fabs((func_res - sum)/func_res);
+    printf("Sum of row with eps {%lf} = %lf\n",eps,sum);
+    printf("Absolute error = %lf\nRelative error = %lf\n",abs_error,relative_error);
+    return EXIT_SUCCESS;
+}
