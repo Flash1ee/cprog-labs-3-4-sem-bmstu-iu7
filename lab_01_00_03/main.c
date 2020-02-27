3ffb79b2c18bf9cfa86b074f6c4eff541809937a
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-02-27 20:35:37 +0300

    Add resolve task 2 + tests for 3 task

---
+++ lab_01_00_03/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    setbuf(stdout, NULL);
+    double high, chest_len, mass;
+    double normal_weight, index_mass;
+    printf("Input high, chest_len,mass: ");
+    if (scanf("%lf", &high) != 1 || scanf("%lf", &chest_len) != 1 || scanf("%lf", &mass) != 1
+        || (high <= 0) || (chest_len < 0) || (mass < 0)) 
+    {
+        printf("Incorrect input\n");
+        return EXIT_FAILURE;
+    }
+    normal_weight = high * chest_len / 240;
+    high = high / 100;
+    index_mass = mass / (high * high);
+    printf("Normal weight = %.5lf\n", normal_weight);
+    printf("Index mass = %.5lf", index_mass);
+    return EXIT_SUCCESS;
+}
