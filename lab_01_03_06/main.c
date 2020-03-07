e3b623111adfb7c1bbb0fe3c62b8173b7229e68d
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-07 12:12:14 +0300

    correction comments about 6 task

--- lab_01_03_06/main.c
+++ lab_01_03_06/main.c
@@ -1,10 +1,7 @@
-/*Определить взаимное расположение точки и прямой: лежит выше прямой, на прямой, под
-прямой.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-long int vector_prod(long ax, long ay, long bx, long by);
+long int dot_product(long ax, long ay, long bx, long by);
 void print_result(long res);
 
 int main(void)
@@ -15,8 +12,12 @@ int main(void)
     long xpoint, ypoint, result;
     printf("Input coords x1 y1 x2 y2 of line ");
     printf("and coords point for check xA yA:\n");
-    if ((scanf("%ld%ld%ld%ld", &x1, &y1, &x2, &y2) != 4) || scanf("%ld%ld", &xpoint, &ypoint) != 2
-        || ((x1 == x2) && (y1 == y2)))
+    if ((scanf("%ld%ld%ld%ld", &x1, &y1, &x2, &y2) != 4) || ((x1 == x2) && (y1 == y2)))
+    {
+        printf("Invalid input\n");
+        return EXIT_FAILURE;
+    }
+    else if (scanf("%ld%ld", &xpoint, &ypoint) != 2)
     {
         printf("Invalid input\n");
         return EXIT_FAILURE;
@@ -31,12 +32,12 @@ int main(void)
         x2 = tmp_x;
         y2 = tmp_y;
     }
-    result = vector_prod(x2 - x1, y2 - y1, xpoint - x1, ypoint - y1);
+    result = dot_product(x2 - x1, y2 - y1, xpoint - x1, ypoint - y1);
     print_result(result);
     return EXIT_SUCCESS;
 }
 
-long int vector_prod(long ax, long ay, long bx, long by)
+long int dot_product(long ax, long ay, long bx, long by)
 {
     long s;
     s = (ax * by - bx * ay);
