745dfcdb56e3e5ff79ad9bcf9428e5dd28c12afc
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-07 11:56:04 +0300

    correction comments about 3 task

--- lab_01_00_03/main.c
+++ lab_01_00_03/main.c
@@ -1,22 +1,23 @@
-/*Определить нормальный вес человека и индекс массы его тела по формулам: h * t / 240 и
-m / h2, где h – рост человека (измеряемый в сантиметрах в первой формуле и в метрах – во
-второй); t – длина окружности грудной клетки (в сантиметрах); m – вес (в килограммах).*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
     setbuf(stdout, NULL);
-    double high, chest_len, mass;
+    double high, chest_girth, mass;
     double normal_weight, index_mass;
-    printf("Input high, chest_len,mass: ");
-    if (scanf("%lf", &high) != 1 || scanf("%lf", &chest_len) != 1 || scanf("%lf", &mass) != 1
-        || (high <= 0) || (chest_len < 0) || (mass < 0)) 
+    printf("Input high, chest_girth,mass: ");
+    if (scanf("%lf", &high) != 1 || scanf("%lf", &chest_girth) != 1 || (high <= 0))
     {
         printf("Incorrect input\n");
         return EXIT_FAILURE;
     }
-    normal_weight = high * chest_len / 240.;
+    if (scanf("%lf", &mass) != 1 || (chest_girth < 0) || (mass < 0)) 
+    {
+        printf("Incorrect input\n");
+        return EXIT_FAILURE;
+    }
+    normal_weight = high * chest_girth / 240.;
     high = high / 100.;
     index_mass = mass / (high * high);
     printf("Normal weight = %.5lf\n", normal_weight);
