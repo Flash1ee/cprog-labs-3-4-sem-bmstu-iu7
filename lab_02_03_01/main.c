f551150176052922ebfed5e412d39549ec6fd77f
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-08 19:02:39 +0300

    change types and names elemetns

--- lab_02_03_01/main.c
+++ lab_02_03_01/main.c
@@ -3,37 +3,37 @@
 #include <math.h>
 #define N 10
 
-int input_pos_massiv(int* mas, int k, int* LenPos);
-float get_pos_element_prod(int* mas, int k);
+int input_pos_massiv(int* mas, int k, int* len);
+double get_pos_element_prod(int* mas, int k);
 int main()
 {
-    int k, A[N], LenPosMas = 0;
+    int k, a[N], len_pos = 0;
     if (scanf("%d", &k) != 1 || k > 10 || k <= 0)
     {
         return EXIT_FAILURE;
     }
-    if (input_pos_massiv(A, k, &LenPosMas) != 0)
+    if (input_pos_massiv(a, k, &len_pos) != 0)
         return EXIT_FAILURE;
-    printf("%f", pow(get_pos_element_prod(A, k), 1.0 / (double)LenPosMas));
+    printf("%lf", pow(get_pos_element_prod(a, k), 1.0 / (double)len_pos));
     return EXIT_SUCCESS;
 }
 
-int input_pos_massiv(int* mas, int k, int* LenPos)
+int input_pos_massiv(int* mas, int k, int* len)
 {
     for (int i = 0; i < k; i++)
     {
         if (scanf("%d", &mas[i]) != 1)
             return EXIT_FAILURE;
         if (mas[i] > 0)
-            *LenPos += 1;
+            *len += 1;
     }
-    if (!*LenPos)
+    if (!*len)
         return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
-float get_pos_element_prod(int* mas, int k)
+double get_pos_element_prod(int* mas, int k)
 {
-    float prod = 1.0;
+    double prod = 1.0;
     for (int i = 0; i < k; i++)
     {
         if (mas[i] > 0)
