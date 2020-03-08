d3d4847d65dae417f1ec27fb9144de2a9cf7f8cc
Dmitry Varin <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-08 18:47:30 +0300

    update task1

--- lab_02_03_01/main.c
+++ lab_02_03_01/main.c
@@ -3,32 +3,31 @@
 #include <math.h>
 #define N 10
 
-int input_pos_massiv(int *mas, int k);
+int input_pos_massiv(int *mas, int k,int *LenPos);
 float get_pos_element_prod(int *mas, int k);
 int main()
 {
-    int k, A[N];
+    int k, A[N],LenPosMas = 0;
     if (scanf("%d", &k)!= 1 || k > 10 || k <= 0)  
     {
         return EXIT_FAILURE;
     }
-    if (input_pos_massiv(A,k)!=0)
+    if (input_pos_massiv(A,k,&LenPosMas)!=0)
         return EXIT_FAILURE;
-    printf("%f",sqrt(get_pos_element_prod(A,k)));
+    printf("%f",pow(get_pos_element_prod(A,k), 1.0 / (double) LenPosMas));
     return EXIT_SUCCESS;
 }
 
-int input_pos_massiv(int *mas,int k)
+int input_pos_massiv(int *mas,int k,int *LenPos)
 {
-    int flag = 0;
     for (int i = 0;i<k;i++)
     {
         if (scanf("%d",&mas[i])!=1)
             return EXIT_FAILURE;
         if (mas[i] > 0)
-            flag = 1;
+            *LenPos +=1;
     }
-    if (!flag)   
+    if (!*LenPos)   
         return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
