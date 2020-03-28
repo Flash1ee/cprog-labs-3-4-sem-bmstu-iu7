85ad60c646de40538d33192e8333c0d63677915e
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-29 00:25:29 +0300

    Task 1 lab_03 fix

--- lab_03_03_01/main.c
+++ lab_03_03_01/main.c
@@ -9,16 +9,16 @@ void output(long long a[], size_t n);
 int monotonic_sequence(long long a[], size_t m);
 void increasing_sequence(long long a[], size_t m, int *flag);
 void decreasing_sequence(long long a[], size_t m, int *flag);
-void vector(long long b[],long long a[][N], size_t n, size_t m);
+void vector(long long b[], long long a[][N], size_t n, size_t m);
 
 int main()
 {
     long long a[N][N];
     size_t n, m;
-    long long b[N*N];
+    long long b[N * N];
 
-    if (scanf("%zu", &n) != 1 || scanf("%zu", &m) != 1 ||
-    n <= 0 || n > 10 || m <= 1 || m > 10)
+    if (scanf("%zu", &n) != 1 || scanf("%zu", &m) != 1 || \
+     n <= 0 || n > 10 || m <= 1 || m > 10)
         return EXIT_FAILURE;
 
     if (input(a, n, m) != 0)
@@ -30,39 +30,26 @@ int main()
     {
         if (monotonic_sequence(a[i], m) == 1)
             b[i] = 1;
-        else 
+        else
             b[i] = 0;
     }
-    output(b,n*m);
+    output(b, n * m);
     return EXIT_SUCCESS;
-
 }
 
 int input(long long a[][N], size_t n, size_t m)
 {
     for (size_t i = 0; i < n; i++)
-        for (size_t j = 0; j < m; j++) 
+        for (size_t j = 0; j < m; j++)
             if (scanf("%lld", &a[i][j]) != 1)
                 return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
-/*
-void output(size_t a[N][N], long n, long m)
+
+void output(long long a[], size_t n)
 {
-    for (size_t i = 0; i < n;i++)
-    {
-        for (size_t j = 0; j < m;j++)
-            printf("%ld ", a[i][j]);
-        printf("\n");
-    }
-}
-*/
-void output(long long *a, size_t n)
-{
-    for (size_t i =0; i<n;i++)
+    for (size_t i = 0; i < n; i++)
         printf("%lld ", a[i]);
-
-
 }
 int monotonic_sequence(long long a[], size_t m)
 {
@@ -73,25 +60,25 @@ int monotonic_sequence(long long a[], size_t m)
 }
 void increasing_sequence(long long a[N], size_t m, int *flag)
 {
-    for  (size_t i = 0; i < m-1; i++)
-            {
-                if (a[i] > a[i+1])
-                return;
-            }
+    for (size_t i = 0; i < m - 1; i++)
+    {
+        if (a[i] > a[i + 1])
+            return;
+    }
     *flag = 1;
 }
 void decreasing_sequence(long long a[N], size_t m, int *flag)
 {
-    for (size_t i = 0; i < m-1; i++)
-        {
-            if (a[i] < a[i+1])
-                return;
-        }
+    for (size_t i = 0; i < m - 1; i++)
+    {
+        if (a[i] < a[i + 1])
+            return;
+    }
     *flag = 1;
 }
-void vector(long long b[],long long a[][N], size_t n, size_t m)
+void vector(long long b[], long long a[][N], size_t n, size_t m)
 {
-    for (size_t i = 0; i < n; i++) 
-        for (size_t j = 0; j<m; j++)
-            b[i*m + j] = a[i][j]; 
+    for (size_t i = 0; i < n; i++)
+        for (size_t j = 0; j < m; j++)
+            b[i * m + j] = a[i][j];
 }
