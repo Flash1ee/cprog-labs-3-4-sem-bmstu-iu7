8699d50727c389e089d3793aa71990d92cd2faa9
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-03-29 11:09:01 +0300

    first vers of task 2

---
+++ lab_03_03_02/main.c
@@ -0,0 +1,101 @@
+#define __USE_MINGW_ANSI_STDIO 1
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 10
+
+int input(long long a[][N], size_t *n, size_t *m);
+void output(long long a[][N], size_t *n, size_t *m);
+void check_matrix(long long a[][N], size_t *n, size_t *m, long k);
+int check_numb(long long x, long k);
+void swap_column(long long a[][N], size_t *column, size_t *count_str, size_t *count_col);
+
+int main()
+{
+    long long a[N][N];
+    size_t n, m;
+    long k;
+
+    if (scanf("%zu", &n) != 1 || scanf("%zu", &m) != 1 || \
+    	n < 1 || n > 10 || m < 1 || m > 10)
+        return EXIT_FAILURE;
+    if (scanf("%ld", &k) != 1 || k < 0 || k > 9)
+        return EXIT_FAILURE;
+    if (input(a, &n, &m) != 0)
+        return EXIT_FAILURE;
+    check_matrix(a,&n,&m,k);
+    output(a, &n, &m);
+    return EXIT_SUCCESS;
+}
+
+int input(long long a[][N], size_t *n, size_t *m)
+{
+    for (size_t i = 0; i < *n; i++)
+        for (size_t j = 0; j < *m; j++)
+            if (scanf("%lld", &a[i][j]) != 1)
+                return EXIT_FAILURE;
+    return EXIT_SUCCESS;
+}
+
+void output(long long a[][N], size_t *n, size_t *m)
+{
+    for (size_t i = 0; i < *n; i++)
+    {
+        for(size_t j = 0; j < *m; j++)
+            printf("%lld ", a[i][j]);
+        printf("\n");
+    }
+}
+
+void check_matrix(long long a[][N], size_t *n, size_t *m, long k)
+{
+    long long x;
+    int flag = 0;
+    size_t i = 0;
+    size_t j = 0;
+    //столбцы
+    while (i < *m)
+    {
+        flag = 0;
+        //строки
+        while (j < *n)
+            {
+                x = a[j][i];
+                if (check_numb(x, k) == 1)
+                {
+                    swap_column(a, &i, n,m);
+                    flag = 1;
+                    *m -= 1;
+                    break;
+                }
+                else
+                    j +=1;
+            }
+        if (!flag)
+            i+=1;
+        j = 0;
+    }
+            
+
+
+}
+int check_numb(long long x, long k)
+{
+    long long digit;
+    if ((x==0)&&(x==k))
+        return 1;
+    while (x>0)
+    {
+        digit = x%10;
+        x/=10;
+        if (digit == k)
+            return 1;
+    }
+    return 0;
+}
+void swap_column(long long a[][N], size_t *column, size_t *count_str, size_t *count_col)
+{
+    for (size_t i = *column + 1; i < *count_col; i++)
+        for (size_t j = 0; j < *count_str; j++)
+            a[j][i-1] = a[j][i];
+}
