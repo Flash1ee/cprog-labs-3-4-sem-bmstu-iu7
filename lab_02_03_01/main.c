03e877ad27c4d8c43497945e9fbdad4a429e78c6
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-04-05 01:12:32 +0300

    upd lab_02_03_02

--- lab_02_03_01/main.c
+++ lab_02_03_01/main.c
@@ -1,15 +1,17 @@
+#define __USE_MINGW_ANSI_STDIO
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #define N 10
 
-int input(long mas[], long n);
-int get_pos(long mas[], long k, long *len);
-double get_pos_element_prod(long mas[], long n);
+int input(long mas[], size_t n);
+int get_pos(long mas[], size_t k, long *len);
+double get_pos_element_prod(long mas[], size_t n);
 int main()
 {
-    long n, a[N], len_pos = 0;
-    if (scanf("%ld", &n) != 1 || n > 10 || n <= 0)
+    size_t n; 
+    long a[N], len_pos = 0;
+    if (scanf("%zu", &n) != 1 || n > 10 || n <= 0)
         return EXIT_FAILURE;
     if (input(a, n) != EXIT_SUCCESS || get_pos(a, n, &len_pos) != EXIT_SUCCESS)
         return EXIT_FAILURE;
@@ -17,9 +19,9 @@ int main()
     return EXIT_SUCCESS;
 }
 
-int get_pos(long mas[], long n, long *len)
+int get_pos(long mas[], size_t n, long *len)
 {
-    for (long i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
         if (mas[i] > 0)
             *len += 1;
     if (!*len)
@@ -27,17 +29,17 @@ int get_pos(long mas[], long n, long *len)
     return EXIT_SUCCESS;
 }
 
-int input(long mas[], long n)
+int input(long mas[], size_t n)
 {
-    for (long i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
         if (scanf("%ld", &mas[i]) != 1)
             return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
-double get_pos_element_prod(long mas[], long n)
+double get_pos_element_prod(long mas[], size_t n)
 {
     double prod = 1.0;
-    for (long i = 0; i < n; i++)
+    for (size_t i = 0; i < n; i++)
         if (mas[i] > 0)
             prod *= mas[i];
     return prod;
--- lab_02_03_02/main.c
+++ lab_02_03_02/main.c
@@ -1,65 +1,77 @@
+#define __USE_MINGW_ANSI_STDIO
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 #define N 10
 
-long input(long a[], long n);
-void output(long a[], long n);
-long armstrong_numb(long a[], long out[], long n);
-long count_digit(long x);
+long input(long a[], size_t n);
+void output(long a[], size_t n);
+int is_armstrong(long elem);
+long count(long x);
+long append_armstrong(long a[], long out[], size_t n);
 
 int main()
 {
-    long n, pos;
+    size_t n, pos;
     long a[N], out[N] = { 0 };
 
-    if (scanf("%ld", &n) != 1 || n <= 0 || n > N)
-    {
+    if (scanf("%zu", &n) != 1 || n <= 0 || n > N)
         return EXIT_FAILURE;
-    }
-    if (input(a, n) != 0)
+
+    if (input(a, n) != EXIT_SUCCESS)
         return EXIT_FAILURE;
-    pos = armstrong_numb(a, out, n);
+
+    pos = append_armstrong(a, out, n);
     if (pos <= 0)
         return EXIT_FAILURE;
+
     output(out, pos);
+
     return EXIT_SUCCESS;
 }
 
 
-long input(long a[], long n)
+long input(long a[], size_t n)
 {
-    for (long i = 0; i < n; i++)
-    {
+    for (size_t i = 0; i < n; i++)
         if (scanf("%ld", &a[i]) != 1)
             return EXIT_FAILURE;
-    }
     return EXIT_SUCCESS;
 }
 
-long armstrong_numb(long a[], long out[], long n)
+int is_armstrong(long elem)
 {
-    long pos = 0;
-    for (long i = 0; i < n; i++)
-    {
-        long k = count_digit(a[i]);
-        long numb_check = 0, cur = a[i];
-        while (cur > 0)
+    long numb_check = 0;
+    long last, cur;
+    long k = count(elem);
+    cur = elem;
+    while (cur > 0)
         {
-            long last = cur % 10;
+            last = cur % 10;
             numb_check += pow(last, k);
             cur /= 10;
         }
-        if (numb_check == a[i])
+    if (numb_check == elem)
+        return EXIT_SUCCESS;
+    return EXIT_FAILURE;
+}
+
+long append_armstrong(long a[], long out[], size_t n)
+{
+    size_t len = 0;
+    for (size_t i = 0; i < n; i++)
+    {
+        if (is_armstrong(a[i]) == EXIT_SUCCESS)
         {
-            out[pos] = a[i];
-            pos += 1;
+            out[len] = a[i];
+            len += 1;
         }
     }
-    return pos;
+    return len;
 }
-long count_digit(long x)
+
+long count(long x)
 {
     long k = 0;
     while (x > 0)
@@ -69,10 +81,9 @@ long count_digit(long x)
     }
     return k;
 }
-void output(long a[], long n)
+
+void output(long a[], size_t n)
 {
-    for (int i = 0; i < n; i++)
-    {
+    for (size_t i = 0; i < n; i++)
         printf("%ld ", a[i]);
-    }
 }
