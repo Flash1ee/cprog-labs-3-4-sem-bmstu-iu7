2d5e1b44bc6c6b6dd54825eadb7528f8fa23565a
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-04-05 02:40:09 +0300

    upd lab_02_03_05

--- lab_02_03_05/main.c
+++ lab_02_03_05/main.c
@@ -1,36 +1,47 @@
+#define __USE_MINGW_ANSI_STDIO
 #include <stdio.h>
 #include <stdlib.h>
 
 #define N 10
 
-long input(long *beg, long *end);
-long search_min_prod(long *start, long *stop, long long *minp);
+int input(long long *beg, long long *end);
+int get_min_prod(long long *start, long long *stop, long long *minp);
+
 int main()
 {
-    long a[N];
-    long n;
-    long *pa_beg = NULL;
-    long *pa_end = NULL;
-    if (scanf("%ld", &n) != 1 || n > N || n <= 0)
+    long long a[N];
+    size_t n;
+    long long minp;
+    long long *pa_beg = NULL;
+    long long *pa_end = NULL;
+
+    if (scanf("%zu", &n) != 1 || n > N || n <= 0)
         return EXIT_FAILURE;
+
     pa_beg = a;
     pa_end = a + n;
-    if (input(pa_beg, pa_end) != 0)
+
+    if (input(pa_beg, pa_end) != EXIT_SUCCESS)
         return EXIT_FAILURE;
-    long long minp = *pa_beg * *(pa_beg + 1);
-    if (search_min_prod(pa_beg, pa_end, &minp) != 0)
+
+    minp = *pa_beg * *(pa_beg + 1);
+    if (get_min_prod(pa_beg, pa_end, &minp) != EXIT_SUCCESS)
         return EXIT_FAILURE;
+
     printf("%lld", minp);
+
     return EXIT_SUCCESS;
 }
-long input(long *beg, long *end)
+
+int input(long long *beg, long long *end)
 {
-    for (long *pcur = beg; pcur < end; pcur++)
-        if (scanf("%ld", pcur) != 1)
+    for (long long *pcur = beg; pcur < end; pcur++)
+        if (scanf("%lld", pcur) != 1)
             return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
-long search_min_prod(long *start, long *stop, long long *minp)
+
+int get_min_prod(long long *start, long long *stop, long long *minp)
 {
     if ((stop - start) <= 1)
         return EXIT_FAILURE;
@@ -39,9 +50,7 @@ long search_min_prod(long *start, long *stop, long long *minp)
         long frst = *start;
         long sec = *(start + 1);
         if (frst * sec < *minp)
-        {
             *minp = frst * sec;
-        }
         start++;
     }
     return EXIT_SUCCESS;
