6e780eb6dc390b2b29f75fd42a651c4501c633da
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-04-06 22:31:28 +0300

    Format code

--- lab_01_00_02/main.c
+++ lab_01_00_02/main.c
@@ -2,11 +2,13 @@
 #include <math.h>
 #include <stdlib.h>
 
-int main(void)
+int main()
 {
     setbuf(stdout, NULL);
+    
     double up_side, down_side, h;
     double perimetr, lateral_side;
+    
     printf("Input up_side, down_side, h:\n");
     if (scanf("%lf", &up_side) != 1 || scanf("%lf", &down_side) != 1 || scanf("%lf", &h) != 1)
     {
@@ -20,6 +22,8 @@ int main(void)
     }
     lateral_side = h * h + (up_side - down_side) * (up_side - down_side) / 4.;
     perimetr = up_side + down_side + sqrt(lateral_side) * 2;
+    
     printf("Perimetr of figure = %.5lf\n", perimetr);
+    
     return EXIT_SUCCESS;
 }
