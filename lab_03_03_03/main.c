c1d33f854a627606b36e188bd148c18d236596e6
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-04-13 10:24:34 +0300

    add matrix - library to task 3

--- lab_03_03_03/main.c
+++ lab_03_03_03/main.c
@@ -1,73 +1,50 @@
 #define __USE_MINGW_ANSI_STDIO 1
 #include <stdio.h>
 #include <stdlib.h>
+#include "array.h"
 
-#define N 10
 #define IND 2
 
-int input(long long a[][N], size_t *n, size_t *m);
-void output(long long a[][N], size_t *n, size_t *m);
-void search_min(long long a[][N], long long min[][N], size_t *n, size_t *m);
-void sort(long long min[][N], size_t *col);
-void swap_str(long long a[][N], long long b[][N], long long min[], size_t *str, size_t *col);
-void swap(long long *a, long long *b);
+void search_min(my_int a[][N], my_int min[][N], size_t n, size_t m);
+void sort(my_int min[][N], size_t col);
+void swap_str(my_int a[][N], my_int b[][N], my_int min[], size_t str, size_t col);
 
 int main()
 {
-    long long a[N][N];
-    long long b[N][N];
-    long long min[IND][N];
+    my_int a[N][N];
+    my_int b[N][N];
+    my_int min[IND][N];
     size_t n, m;
 
     if (scanf("%zu", &n) != 1 || scanf("%zu", &m) != 1 || n < 1 || n > N || m < 1 || m > N)
         return EXIT_FAILURE;
-    if (input(a, &n, &m) != 0)
+    if (input(a, n, m) != 0)
         return EXIT_FAILURE;
         
-    search_min(a, min, &n, &m);
-    sort(min, &n);
-    swap_str(a, b, min[1], &n, &m);
-    output(b, &n, &m);
+    search_min(a, min, n, m);
+    sort(min, n);
+    swap_str(a, b, min[1], n, m);
+    output(b, n, m);
     
     return EXIT_SUCCESS;
 }
 
-int input(long long a[][N], size_t *n, size_t *m)
+void search_min(my_int a[][N], my_int min[][N], size_t n, size_t m)
 {
-    for (size_t i = 0; i < *n; i++)
-        for (size_t j = 0; j < *m; j++)
-            if (scanf("%lld", &a[i][j]) != 1)
-                return EXIT_FAILURE;
-                
-    return EXIT_SUCCESS;
-}
-
-void output(long long a[][N], size_t *n, size_t *m)
-{
-    for (size_t i = 0; i < *n; i++)
-    {
-        for (size_t j = 0; j < *m; j++)
-            printf("%lld ", a[i][j]);
-        printf("\n");
-    }
-}
-
-void search_min(long long a[][N], long long min[][N], size_t *n, size_t *m)
-{
-    for (size_t i = 0; i < *n; i++)
+    for (size_t i = 0; i < n; i++)
     {
         min[0][i] = a[i][0];
         min[1][i] = i;
-        for (size_t j = 0; j < *m; j++)
+        for (size_t j = 0; j < m; j++)
             if (a[i][j] < min[0][i])
                 min[0][i] = a[i][j];
     }
 }
 
-void sort(long long min[][N], size_t *col)
+void sort(my_int min[][N], size_t col)
 {
-    for (size_t i = 0; i < *col - 1; i++)
-        for (size_t j = 0; j < *col - i - 1; j++)
+    for (size_t i = 0; i < col - 1; i++)
+        for (size_t j = 0; j < col - i - 1; j++)
         {
             if (min[0][j] < min[0][j + 1])
             {
@@ -77,21 +54,13 @@ void sort(long long min[][N], size_t *col)
         }
 }
 
-void swap_str(long long a[][N], long long b[][N], long long min[], size_t *str, size_t *col)
+void swap_str(my_int a[][N], my_int b[][N], my_int min[], size_t str, size_t col)
 {
-    long long cur_str;
-    for (size_t i = 0; i < *str; i++)
+    my_int cur_str;
+    for (size_t i = 0; i < str; i++)
     {
         cur_str = min[i];
-        for (size_t j = 0; j < *col; j++)
+        for (size_t j = 0; j < col; j++)
             b[i][j] = a[cur_str][j];
     }
-}
-
-void swap(long long *a, long long *b)
-{
-    long long tmp;
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
 }
--- lab_03_03_03/make_release.cmd
+++ lab_03_03_03/make_release.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
 CHCP 65001
 gcc -std=c99 -Wall -Wpedantic -Wextra -Werror -c main.c
-gcc -o app.exe main.o
+gcc -std=c99 -Wall -Wpedantic -Wextra -Werror -c array.c
+gcc -o app.exe main.o array.o
