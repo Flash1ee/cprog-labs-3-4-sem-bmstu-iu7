d7037b850ee528a6ce19ad8339a198b0c41b302b
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-05-16 20:05:23 +0300

    add task 3

--- lab_51_03/main.c
+++ lab_51_03/main.c
@@ -1,8 +1,5 @@
 #define __USE_MINGW_ANSI_STDIO 1
-#include <stdio.h>
-#include <stdlib.h>
 #include "utils.h"
-#include <time.h>
 int main(int argc, char *argv[])
 {
 
@@ -16,13 +13,13 @@ int main(int argc, char *argv[])
 
     size_t cnt;
     int rc;
-    
+
     if (argc == 4)
     {
-        if (!(cnt = strtol(argv[2],NULL, 10)))
+        if (!(cnt = strtol(argv[2], NULL, 10)))
             return ERR_CNT;
-        
-        FILE *f;
+
+        FILE* f;
         f = fopen(argv[3], "w");
 
         if (write_rand(f, cnt))
@@ -33,14 +30,14 @@ int main(int argc, char *argv[])
         else
             fclose(f);
     }
-    else 
+    else
     {
         if (mode == 'p')
         {
             size_t size;
             FILE *f;
             f = fopen(argv[2], "r");
-            
+
             if (file_size(f, &size))
             {
                 fclose(f);
@@ -53,53 +50,26 @@ int main(int argc, char *argv[])
                 return rc;
             }
             fclose(f);
-        }    
-    }
-
-    return OK;
-}
-
-int write_rand(FILE *f, size_t cnt)
-{
-    srand(time(NULL));
-    int buf;
-    for (size_t i = 0; i < cnt; i++)
-    {
-        buf = (rand() % DIGITS * 2 ) - DIGITS;
-        if (fwrite(&buf, sizeof(int), 1, f) != 1)
-            return WRITE_ERR;
-    }
-    return OK;
-}
-
-int print(FILE *f, size_t size)
-{
-    printf("%zu\n", size);
-    if (!size)
-        return SIZE_ERR;
-    int num;
-    int rc;
-    for (size_t i = 0; i < size / sizeof(int); i++)
-    {
-        rc = fread(&num, sizeof(int), 1, f);
-        if (rc == 1)
-            printf("%d ", num);
+        }
         else
-            return READ_ERR;
+        {
+            size_t size;
+            FILE *f;
+            f = fopen(argv[2], "rb+");
+            if (file_size(f, &size))
+            {
+                fclose(f);
+                return READ_ERR;
+            }
+            for (size_t i = 0; i < size - sizeof(int); i += sizeof(int))
+                for (size_t j = 0; j < size - i - sizeof(int); j += sizeof(int))
+                    if (bubble_sort(f, j))
+                    {
+                        fclose(f);
+                        return SORT_ERR;
+                    }
+            fclose(f);
+        }
     }
-    return OK;
-}
-
-
-int file_size(FILE *f , size_t *size)
-{
-    long tmp;
-    if (fseek(f, 0, SEEK_END))
-        return READ_ERR;
-    tmp = ftell(f);
-    if (tmp < 0)
-        return READ_ERR;
-    *size = tmp;
-    fseek(f, 0, SEEK_SET);
     return OK;
 }
