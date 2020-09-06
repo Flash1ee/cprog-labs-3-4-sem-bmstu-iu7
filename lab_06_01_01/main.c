7d5af79b0ee613cb48deb0cf93e498bad1d241f8
Varin Dmitriy <891-vdv19u755@users.noreply.git.iu7.bmstu.ru>
2020-09-06 22:10:26 +0300

    fix size of string

--- lab_06_01_01/main.c
+++ lab_06_01_01/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 
     if (strcmp(argv[2], "title") && strcmp(argv[2], "name") && strcmp(argv[2], "year"))
         return ARG_ERR;
-    if (argc == 4 && strlen(argv[3]) > 25)
+    if (argc == 4 && strlen(argv[3]) > N - 1)
         return ARG_ERR;
 
     size_t key = 0;
