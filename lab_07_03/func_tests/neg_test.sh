f4a08d739effd861df3ae236826ea22466fb8109
Dmitry Varin <varindv@gmail.com>
2020-10-05 15:16:45 +0300

    fix scripts

--- lab_07_03/func_tests/neg_test.sh
+++ lab_07_03/func_tests/neg_test.sh
@@ -3,10 +3,10 @@
 if [ $# != 1 ]
 then echo ERROR COUNT OF PARAMETERS
 fi
-var=<./tests/neg_$1_args
-../app.exe $var <./tests/pos_$1_in > out.txt
+var=<./tests/neg_$1_args.txt
+../app.exe $var <./tests/pos_$1_in.txt > out.txt
 if [ $? -eq 0 ]
 then echo NEG TEST $1 ::FAIL:: RETURN CODE IS ZERO...
 else echo NEG TEST $1 ::SUCCESS::
 fi 
-diff -u ./shared/neg_$1_res ./shared/neg_$1_out
+diff -u ./shared/neg_$1_res.txt ./shared/neg_$1_out.txt
--- lab_07_03/func_tests/pos_test.sh
+++ lab_07_03/func_tests/pos_test.sh
@@ -3,10 +3,10 @@
 if [ $# != 1 ]
 then echo ERROR COUNT OF PARAMETERS
 fi
-var=$(cat ./tests/pos_$1_args)
-../app.exe $var<./tests/pos_$1_in > out.txt
+var=$(cat ./tests/pos_$1_args.txt)
+../app.exe $var<./tests/pos_$1_in.txt > out.txt
 if [ $? -eq 0 ]
 then echo POS TEST $1 ::SUCCESS::
 else echo POS TEST $1 ::FAIL:: RETURN CODE IS NOT ZERO...
 fi 
-diff -u ./shared/pos_$1_res ./shared/pos_$1_out
+diff -u ./shared/pos_$1_res.txt ./shared/pos_$1_out.txt
