be3acb302597d5ae64440d27943d4aed32c10fb2
Dmitry Varin <varindv@gmail.com>
2020-11-03 22:18:22 +0300

    add sec task without tests and makefile

---
+++ mct_03_02_02/task_2/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "matrix.h"
+
+int main()
+{
+    size_t rows, cols;
+    if (scanf("%zu", &rows) != 1 || scanf("%zu", &cols) != 1)
+    {
+        return READ_ERR;
+    }
+    matrix_t *matrix = allocate_matrix(rows, cols);
+    if (!matrix)
+    {
+        return ALLOCATION_ERR;
+    }
+    if (input(matrix))
+    {
+        free_matrix(matrix);
+        return READ_ERR;
+    }
+
+    output(matrix);
+    
+    del_max_col(matrix);
+    
+    printf("\n");
+    output(matrix);
+    
+    free_matrix(matrix);
+
+    return EXIT_SUCCESS;
+}
--- mct_03_02_02/task_2/src/matrix.c
+++ mct_03_02_02/task_2/src/matrix.c
@@ -7,7 +7,7 @@ matrix_t *allocate_matrix(size_t rows, size_t cols)
     {
         return NULL;
     }
-    double **data = calloc(rows, sizeof(double*));
+    num_t **data = calloc(rows, sizeof(num_t*));
     if (!data)
     {
         free(temp);
@@ -15,13 +15,14 @@ matrix_t *allocate_matrix(size_t rows, size_t cols)
     }
     for (size_t i = 0; i < rows; i++)
     {
-        data[i] = malloc(sizeof(double) * cols);
+        data[i] = malloc(sizeof(num_t) * cols);
         if (!data)
         {
             free_matrix(temp);
             return NULL;
         }
     }
+    temp->data = data;
     temp->rows = rows;
     temp->cols = cols;
     return temp;
@@ -33,4 +34,63 @@ void free_matrix(matrix_t *arr)
         free(arr->data[i]);
     }
     free(arr);
+}
+int input(matrix_t *arr)
+{
+    for (size_t i = 0; i < arr->rows; i++)
+    {
+        for (size_t j = 0; j < arr->cols; j++)
+        {
+            if (scanf(ScNum, &(arr->data[i][j])) != 1)
+            {
+                return READ_ERR;
+            }
+        }
+    }
+    return EXIT_SUCCESS;
+}
+void output(matrix_t *arr)
+{
+    for (size_t i = 0; i < arr->rows; i++)
+    {
+        for (size_t j = 0; j < arr->cols; j++)
+        {
+            printf(PrNum" ", arr->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+void del_max_col(matrix_t *arr)
+{
+    size_t delete_col = get_max_col(arr);
+    for (size_t i = 0; i < arr->rows; i++)
+    {
+        for (size_t j = delete_col + 1; j < arr->cols; j++)
+        {
+            arr->data[i][j-1] = arr->data[i][j];
+        }
+    }
+    arr->cols -= 1;
+}
+size_t get_max_col(matrix_t *arr)
+{
+    size_t i_max = 0;
+    size_t max_sum = 0;
+    size_t cur_sum = 0;
+
+    for (size_t i = 0; i < arr->cols; i++)
+    {
+        cur_sum = 0;
+
+        for (size_t j = 0; j < arr->rows; j++)
+        {
+            cur_sum += arr->data[j][i];
+        }
+        if (cur_sum > max_sum)
+        {
+            max_sum = cur_sum;
+            i_max = i;
+        }
+    }
+    return i_max;
 }
