33a7b0e42219db8b76687cbf466a0f2a6ca444cb
Dmitry Varin <varindv@gmail.com>
2020-12-19 22:16:20 +0300

    replace text widget on listbox + add user friendly messages

--- lab_12_03_02/app.py
+++ lab_12_03_02/app.py
@@ -6,18 +6,19 @@ import random
 
 LIB_ARR = "./lib/libarr.so"
 FUNCS = ["Массив простых", "Фильтрация массива_1", "Фильтрация массива_2"]
-libc = ctypes.CDLL(LIB_ARR)
 
-#int fill_prime(int *arr, int n)
 def import_library_func():
+    #int fill_prime(int *arr, int n)
     fill_prime = libc.fill_prime
     fill_prime.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int)
     fill_prime.restype = ctypes.c_int
 
+    #int form_arr_user_size(int *src, int src_n, int *dst, int dst_n)
     form_arr_user_size= libc.form_arr_user_size
     form_arr_user_size.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.POINTER(ctypes.c_int), ctypes.c_int)
     form_arr_user_size.restype = ctypes.c_int
 
+    #int form_arr_my_size(int *src, int src_n, int *dst, int *dst_n)
     form_arr_my_size = libc.form_arr_my_size
     form_arr_my_size.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
     form_arr_my_size.restype = ctypes.c_int
@@ -29,6 +30,9 @@ def import_library_func():
 def show_help():
     new_window = tk.Toplevel()
     new_window.title("Справка")
+    new_window.resizable(width = False, height = False)
+
+
     text = [["---------------","Программа для работы с библиотекой libarr.so", "---------------"],
         ["Си-функции","Python-функции","Описание"],
         ["int fill_prime(int *arr, int n)","fill_prime(arr)","Заполнение массива простыми числами"],
@@ -55,13 +59,9 @@ def show_help():
             label.pack(padx=5, pady=5)
 
     tk.Label(new_window, text = "Количество данных на вызывающей стороне,"
-    "при вводе больших значений возможны задержки выполнения").grid(row = len(text), column = 0, columnspan = 3)
+    "при вводе больших значений возможны задержки выполнения").grid(row = len(text), column = 0, columnspan = 3, sticky = tk.W)
+    tk.Label(new_window, text = "Разработчик : Дмитрий Варин ИУ7-36Б").grid(row = len(text) + 1, column = 0, sticky = tk.W)
     
-    
-
-def form_menu(main_menu : tk.Menu):
-    main_menu.add_command(label = "Справка", command = show_help)
-
 
 def fill_prime(n):
     arr = (ctypes.c_int * n)()
@@ -121,6 +121,11 @@ def random_fill():
     else:
         my_entry.insert(1.0," ".join(str(i) for i in [random.randint(1,50) for j in range(10)]))
 
+def output(res):
+    my_result.delete(0, my_result.size())
+    for i in range(len(res)):
+            my_result.insert(i, f"index = {i} elem = {str(res[i])}")
+
 
 def calculate():
     cur_value = func_list.get()
@@ -130,11 +135,7 @@ def calculate():
             messagebox.showinfo("Вас заметили", "Ввод некорректен")
             return
         n = int(user_input[0])
-        res = fill_prime(n)
-        my_result.configure(state=tk.NORMAL)
-        my_result.delete(1.0, tk.END)
-        my_result.insert(1.0, str(res))
-        my_result.configure(state=tk.DISABLED)
+        output(fill_prime(n))
 
     elif (cur_value == FUNCS[1] or cur_value == FUNCS[2]):
         user_input = my_entry.get(1.0, tk.END).split()
@@ -142,42 +143,41 @@ def calculate():
             if (not i.isdecimal()):
                 messagebox.showinfo("Вас заметили", "Ввод некорректен")
                 return
+
         src = [int(num) for num in user_input]
-        print(src)
         if (cur_value == FUNCS[1]):
             res = form_arr_by_even(src)
         else:
             res = form_arr_by_even_eco(src)
-        # res = " ".join(i for i in form_arr_by_even(src))
-        my_result.configure(state=tk.NORMAL)
-        my_result.delete(1.0, tk.END)
-        my_result.insert(1.0, str(res))
-        my_result.configure(state=tk.DISABLED)
+        output(res)
     else:
         messagebox.showinfo("Вас заметили", "Ввод некорректен")
-        
+
+
+libc = ctypes.CDLL(LIB_ARR)
+_fill_prime, _form_arr_user, _form_arr_my = import_library_func()
+
 window = tk.Tk()
+window.title("Работа с библиотекой импортированной из СИ")
 window.resizable(width = False, height = False)
+
+messagebox.showinfo("Предупреждение", "Перед началом работы соберите библиотеку\nВведите make lib из текущей директории")
+
 tk.Label(text="Функция").grid(row=0, column=0, sticky=tk.W, pady=10, padx=10)
-func_list = ttk.Combobox(values=["Массив простых",
-                            "Фильтрация массива_1",
-                            "Фильтрация массива_2",
-                            ],
-                            postcommand=change_func)
-
-
+func_list = ttk.Combobox(values=["Массив простых","Фильтрация массива_1","Фильтрация массива_2",],postcommand=change_func)
 func_list.grid(row=0, column=1,columnspan=3,sticky=tk.W+tk.E, padx=10)
  
-tk.Label(text="Входные").grid(row = 1, column = 0, rowspan = 2, columnspan = 1,  sticky=tk.W, pady=10, padx=10)
 
+tk.Label(text="Входные").grid(row = 1, column = 0, rowspan = 2, columnspan = 1,  sticky=tk.W, pady=10, padx=10)
 my_entry = tk.Text(width = 60, height = 4)
 my_entry.grid(row = 1, column = 1, rowspan = 2, columnspan = 3, sticky = tk.W + tk.E, padx=10)
 
 
 tk.Button(text = "Запуск", command = calculate).grid(row = 3, column = 2, sticky = tk.W + tk.E)
 tk.Label(text="Выходные").grid(row=4, column=0, rowspan = 2, sticky = tk.W, pady = 10, padx = 10)
-my_result = tk.Text(width = 60, height = 4, state = tk.DISABLED)
-my_result.grid(row = 4, column = 1, columnspan = 3) #перед добавлением state = "normal"
+my_result = tk.Listbox(width = 60, height = 4)
+my_result.grid(row = 4, column = 1, columnspan = 3) 
+
 
 scroll_y = tk.Scrollbar(command=my_result.yview)
 scroll_x = tk.Scrollbar(orient='horizontal')
@@ -193,27 +193,6 @@ tk.Button(text="Справка", command = show_help).grid(row=6, column=0, pady
 tk.Button(text="Случайное заполнение", command = random_fill).grid(row=6, column=2)
 tk.Button(text="Выход", command = exit_app).grid(row=6, column=3)
 
-main_menu = tk.Menu(window)
-form_menu(main_menu)
-
-_fill_prime, _form_arr_user, _form_arr_my = import_library_func()
-
-print(_fill_prime, _form_arr_my, _form_arr_user)
-
-print(fill_prime(5))
-print(form_arr_by_even([2,3,4,5,6,7]))
-print(form_arr_by_even([]))
-print(form_arr_by_even([1,1,1,1,1]))
-
-print(form_arr_by_even_eco([2,3,4,5,6,7]))
-print(form_arr_by_even_eco([]))
-print(form_arr_by_even_eco([1,1,2,1,1]))
-print(form_arr_by_even_eco([2,2,2,2,22]))
-
-
-
-
-window.config(menu=main_menu)
 
 
 window.mainloop()
